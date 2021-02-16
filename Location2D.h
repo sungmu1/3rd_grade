c7e4d8489c8e6e9474f55539a2d77280cd91e21c
sungmu1 <61034399+sungmu1@users.noreply.github.com>
2021-02-16 23:46:07 +0900

    Add files via upload

---
+++ Location2D.h
@@ -0,0 +1,17 @@
+/* 저장할 스택의 형식 */
+
+#pragma once
+#include <cstdio>
+
+class Location2D
+{
+public:
+	int row;
+	int col;
+	Location2D();
+	Location2D(int r = 0 , int c = 0) { 
+		row = r;
+		col = c;
+	}
+	~Location2D(void) { }
+};
